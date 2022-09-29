3312a9f51a1ab70ed66fcd3de72ad128bd8614ca
a.shiryaev <a.shiryaev@siberian.capital>
2022-09-29 21:01:00 +0500

    add task 1

---
+++ 1.3/1.py
@@ -0,0 +1,54 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://spb.hh.ru/search/vacancy?text=python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post&area=2"
+
+HEADERS = {
+    # "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+resp = requests.get(URL, headers=HEADERS)
+
+if resp.status_code != 200:
+    print("error: ", resp.status_code)
+    exit(1)
+
+soup = BeautifulSoup(resp.text, "html.parser")
+
+# print(soup.prettify())
+
+items = soup.find_all("div", class_="vacancy-serp-item__layout")
+
+data = []
+
+for item in items:
+    title_tag = item.find("a", class_="serp-item__title")
+    url = title_tag['href']
+    title = title_tag.text
+    city = item.find("div", {"data-qa": "vacancy-serp__vacancy-address"}).text
+    salary_tag = item.find("span", {"data-qa": "vacancy-serp__vacancy-compensation"})
+    if salary_tag != None:
+        salary = salary_tag.text
+    else:
+        salary = "Salary unknown"
+
+    page = requests.get(url, headers=HEADERS)
+    if page.status_code != 200:
+        experience = "?"
+    else:
+        page_soup = BeautifulSoup(page.text, "html.parser")
+        experience = page_soup.find("span", {"data-qa": "vacancy-experience"}).text
+
+    item = {
+        "title": title,
+        "work experience": experience,
+        "salary": salary,
+        "region": city
+    }
+    data.append(item)
+    # print(f"{title} {city} {salary} {experience}")
+
+with open("res.json", "w") as rf:
+    rf.write(json.dumps(data, indent=4))
